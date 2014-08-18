bed85b3f77df05a1b19fa63b0d5dd4db88522009
Y.Dziashkevich <Y.Dziashkevich@gmail.com>
2014-08-19 00:57:05 +0300

    added items in the database

--- dump/shop.sql
+++ dump/shop.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Ultimate v9.02 
-MySQL - 5.5.25 : Database - shop_st
+SQLyog Ultimate v11.5 (64 bit)
+MySQL - 5.5.37-log : Database - shop_st
 *********************************************************************
 */
 
@@ -23,11 +23,14 @@ DROP TABLE IF EXISTS `st_category`;
 CREATE TABLE `st_category` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `description` text,
+  `description` text NOT NULL,
+  `img` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 /*Data for the table `st_category` */
+
+insert  into `st_category`(`id`,`name`,`description`,`img`) values (1,'cpu','Центра́льный проце́ссор (ЦП; также центральное процессорное устройство — ЦПУ; англ. central processing unit, CPU, дословно — центральное обрабатывающее устройство)','img/cpu/cpu.jpg'),(3,'videokarts','Видеока́рта (также видео ка́рта, видеоада́птер, графический ада́птер, графи́ческая пла́та, графи́ческая ка́рта, графи́ческий ускори́тель, 3D-ка́рта)  — электронное устройство, преобразующее графический образ, хранящийся как содержимое памяти компьютера (или самого адаптера), в форму, пригодную для дальнейшего вывода на экран монитора. Первые мониторы, построенные на электронно-лучевых трубках, работали по телевизионному принципу сканирования экрана электронным лучом, и для отображения требовался видеосигнал, генерируемый видеокартой.','img/videokart/videokart.jpg'),(4,'hdd','Накопи́тель на жёстких магни́тных ди́сках или НЖМД (англ. hard (magnetic) disk drive, HDD, HMDD), жёсткий диск, в компьютерном сленге «винче́стер» — запоминающее устройство (устройство хранения информации) произвольного доступа, основанное на принципе магнитной записи. Является основным накопителем данных в большинстве компьютеров.','img/hdd/hdd.jpg'),(5,'cooller','','img/cooller/cooller.jpg'),(6,'mb','Материнская плата (англ. motherboard, MB; также mainboard, сленг. мама, мать, материнка) — сложная многослойная печатная плата, являющаяся основой построения вычислительной системы (компьютера).','img/mb/mb.jpg'),(7,'ram','Операти́вная па́мять (англ. Random Access Memory, RAM, память с произвольным доступом; ОЗУ; комп. жарг. память, оперативка) — энергозависимая часть системы компьютерной памяти, в которой временно хранятся входные, выходные и промежуточные данные программы процессора. Наиболее распространенные типы DIMM и SIMM.','img/ram/ram.jpg'),(8,'body','','img/body/body.jpg'),(9,'powerSupply','','img/powerSupply/powerSupply.jpg');
 
 /*Table structure for table `st_items` */
 
@@ -81,6 +84,8 @@ CREATE TABLE `st_product2property` (
 
 /*Data for the table `st_product2property` */
 
+insert  into `st_product2property`(`idProduct`,`idProperty`,`value`) values (1,1,'3000'),(1,2,'2'),(1,3,'6'),(2,1,'5000'),(2,2,'6'),(2,3,'10'),(3,1,'2500'),(3,2,'3'),(3,3,'2'),(7,4,'400'),(7,5,'1024'),(8,4,'300'),(8,5,'512'),(10,4,'500'),(10,5,'1024'),(12,6,'hdd'),(12,7,'1024'),(12,8,'24'),(14,6,'hdd'),(14,7,'512'),(14,8,'6'),(13,6,'ssd'),(13,7,'512'),(13,8,'10'),(15,9,'LGA775, LGA1366, LGA1150, LGA1155, LGA1156, AM2, AM2+, AM3, AM3+, FM1, FM2'),(15,10,'21 dBA '),(16,9,'LGA775, LGA1366, LGA1155, LGA1156, AM2, AM2+, AM3'),(16,10,'19.8 dBA '),(17,9,''),(17,10,'20.5 dBA '),(18,11,'Intel \r\n'),(18,9,' LGA1366'),(20,11,'AMD '),(20,9,'AM3 '),(21,11,'Intel '),(21,9,'LGA1155 '),(22,7,'1024'),(22,1,'100'),(23,7,'512'),(23,1,'200'),(24,7,'2048'),(24,1,'300'),(25,12,'mini-ATX '),(25,13,'top'),(26,12,''),(26,13,''),(28,12,''),(28,13,'bottom'),(29,14,'500'),(30,14,''),(32,14,'750');
+
 /*Table structure for table `st_products` */
 
 DROP TABLE IF EXISTS `st_products`;
@@ -91,12 +96,15 @@ CREATE TABLE `st_products` (
   `description` text NOT NULL,
   `price` float DEFAULT NULL,
   `idCategory` int(11) unsigned NOT NULL,
+  `img` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `product2category` (`idCategory`),
   CONSTRAINT `product2category` FOREIGN KEY (`idCategory`) REFERENCES `st_category` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 
 /*Data for the table `st_products` */
+
+insert  into `st_products`(`id`,`name`,`description`,`price`,`idCategory`,`img`) values (1,'Intel Core i5-4570','2013 г. (Q2) Core i5 \r\nHaswell ',5,1,'img/cpu/i5.jpg'),(2,'Intel Core i7-4790K','2014 г. (Q2) Core i7 Devil\'s Canyon LGA1150 ',7,1,'img/cpu/i7.jpg'),(3,'AMD A8-6600K','2013 г. (Q2) A8  Richland  FM2 ',4,1,'img/cpu/a8.jpg'),(7,'MSI GeForce GTX 760','2013 г.  PCI Express x16 3.0  NVIDIA GeForce GTX 760 ',10,3,'img/videokart/gtx760oc.jpg'),(8,'ASUS GeForce GTX 650 Ti','2012 г. PCI Express x16 3.0 NVIDIA GeForce GTX 650 Ti \r\n ',8,3,'img/videokart/gtx650.jpg'),(10,'ASUS GeForce GTX 750 Ti','2014 г. \r\nPCI Express x16 3.0 NVIDIA GeForce GTX 750 Ti ',6,3,'img/videokart/gtx750ti.jpg'),(12,'Seagate Enterprise Capacity 6TB','2014 г. \r\n',2,4,'img/hdd/enterprise.jpg'),(13,'SSD Silicon-Power Extreme','2012 г. \r\n',3,4,'img/hdd/ssdSiliconPower.jpg'),(14,'Seagate Barracuda 7200.14 1TB','2011 г. ',4,4,'img/hdd/seagateBarracuda.jpg'),(15,'Thermalright HR-02 Macho','для процессора  воздушное ',1,5,'img/cooller/thermalright.jpg'),(16,'Noctua NH-D14','для процессора воздушное ',2,5,'img/cooller/noctua.jpg'),(17,'Xilence RedWing COO-XPF120.R','для корпуса воздушное ',1,5,'img/cooller/xilence.jpg'),(18,'ASUS Rampage IV Extreme','2011 г. ',3,6,'img/mb/rampage.jpg'),(20,'BIOSTAR N68S3B Ver. 6.x','',2,6,'img/mb/biostar.jpg'),(21,'ASUS P8Z77-V LX','2012 г. ',4,6,'img/mb/p8z77.jpg'),(22,'Hynix DDR2 PC2-6400 2 Гб','',2,7,'img/ram/hynix.jpg'),(23,'Kingston HyperX Genesis','',3,7,'img/ram/kingston.jpg'),(24,'Corsair Vengeance Blue 2x4GB','',3,7,'img/ram/corsair.jpg'),(25,'Zalman Z11 plus Black','',5,8,'img/body/zalman.jpg'),(26,'In Win GT1 BWR145 Black','',7,8,'img/body/bwr145.jpg'),(28,'Cooler Master Elite 110','',8,8,'img/body/masterElite.jpg'),(29,'Zalman ZM500-LE 500W','',5,9,'img/powerSupply/zalman.jpg'),(30,'Chieftec Navitas GPM-850C','',5,9,'img/powerSupply/navitas.jpg'),(32,'Chieftec A-135 APS-750CB','',7,9,'img/powerSupply/750c.jpg');
 
 /*Table structure for table `st_properties` */
 
@@ -109,9 +117,11 @@ CREATE TABLE `st_properties` (
   PRIMARY KEY (`id`),
   KEY `property2category` (`idCategory`),
   CONSTRAINT `property2category` FOREIGN KEY (`idCategory`) REFERENCES `st_category` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 /*Data for the table `st_properties` */
+
+insert  into `st_properties`(`id`,`property`,`idCategory`) values (1,'frequency',1),(2,'number of cores',1),(3,'cache',1),(4,'gpu frequency',3),(5,'video Memory',3),(6,'type of drive',4),(7,'volume',4),(8,'buffer',4),(9,'socket',5),(10,'noise',5),(11,'cpu',6),(12,'form factor motherboard',8),(13,'location of the power supply',8),(14,'power',9);
 
 /*Table structure for table `st_users` */
 
