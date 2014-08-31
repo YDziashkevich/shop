b6b597518bd0009ea6f0a08194236170dc7fbdec
Y.Dziashkevich <Y.Dziashkevich@gmail.com>
2014-08-31 17:30:38 +0300

    add header, footer

---
+++ inc/views/footer.php
@@ -0,0 +1,11 @@
+<div id="templatemo_footer">
+    <div class="cleaner h10"></div>
+    <p align="center">
+        Copyright © 2014 | Sergey Aristov & Yury Dziashkevich
+    </p>
+</div> <!-- END of footer -->
+
+</div>
+
+</body>
+</html>
