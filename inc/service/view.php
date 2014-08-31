cf7f9c46c3548549f4c04b2fd6759d6558009d2b
Y.Dziashkevich <Y.Dziashkevich@gmail.com>
2014-08-31 17:38:52 +0300

    add footer, header

--- inc/service/view.php
+++ inc/service/view.php
@@ -3,29 +3,9 @@
 class View
 {
     public static $category = array();
-
-    public function __construct()
-    {
-
-    }
     /**
      * @param $name имя шаблона для вывода
      * @param array $data данные для заполнения шаблона
-     */
-    public function renderPartial($name, $data = array())
-    {
-        if(!empty($data)){
-            extract($data);
-        }
-
-        self::$category = CategoryListModel::getCategoryList();
-        //require("inc/views/header.php");
-        require("inc/views/" . $name . ".php");
-        //require("inc/views/footer.php");
-    }
-
-    /**
-     * @param $name имя шаблона, который надо отобразить, вида (user/index)
      */
     public function render($name, $data = array())
     {
@@ -36,6 +16,8 @@ class View
          * массив с списком имени и id категорий
          */
         self::$category = CategoryListModel::getCategoryList();
+        require("inc/views/header.php");
         require('inc/views/'.$name.'.php');
+        require("inc/views/footer.php");
     }
 }
