4bd4d71024dda52fe7be0f681667f1e859af8935
zlucan <zlucan88@rambler.ru>
2014-09-03 00:56:20 +0300

    change header

--- inc/views/header.php
+++ inc/views/header.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Web Store Theme - Free CSS Templates</title>
-    <meta name="keywords" content="web store, free templates, website templates, CSS, HTML" />
-    <meta name="description" content="Web Store Theme - free CSS template provided by templatemo.com" />
+    <title>E-Shop</title>
     <link href="<?php echo APP_BASE_URL; ?>css/templatemo_style.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo APP_BASE_URL; ?>css/bootstrap.css" rel="stylesheet" type="text/css" />
 
