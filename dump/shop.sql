2bb32212088e25248a40a62d7c1133ac30a17846
zlucan <zlucan88@rambler.ru>
2014-09-03 10:12:27 +0300

    Revert "create new table for guestbook"
    This reverts commit 33671a3e2dd5d5e106c7ce24b5de3113c2030514.

--- dump/shop.sql
+++ dump/shop.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Ultimate v9.02 
-MySQL - 5.5.25 : Database - shop_st
+SQLyog Ultimate v11.5 (64 bit)
+MySQL - 5.5.37-log : Database - shop_st
 *********************************************************************
 */
 
@@ -68,23 +68,6 @@ CREATE TABLE `st_items` (
 
 /*Data for the table `st_items` */
 
-/*Table structure for table `st_messages` */
-
-DROP TABLE IF EXISTS `st_messages`;
-
-CREATE TABLE `st_messages` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `message` text NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
-/*Data for the table `st_messages` */
-
-insert  into `st_messages`(`id`,`name`,`email`,`message`,`time`) values (1,'Андрей','andry@mail.ru','Привет, как дела?','2014-09-03 09:04:53'),(2,'Пример текста','sdfsdf@fsdf.ff','Пример текстаПример текстаПример текстаПример текста','2014-09-03 09:42:07'),(3,'Пример текста','sf@fs.rl','Пример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текстаПример текста','2014-09-03 09:43:17'),(4,'sajf dlkjasd','sdfsdf@fsdf.ff','l;kjds ;lakfjs ;laskd f;alsdkf','2014-09-03 09:44:54'),(5,'asdf asdf adsf','sdfsdf@fsdf.ff','kla jd;flkaj ;sldkj a;sldfjas;dl kjas;dfl kjasdf;akl sd;alsdja;sdlk','2014-09-03 09:45:42'),(6,'qqqqq','qwerty@mail.ru','qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq qqqqqqqqqqqqqqqqq q q','2014-09-03 09:47:13'),(7,'qwer','qwerty@mail.ru','qwer qwer qwer qwr qw r qwer qwe qwer qwe qew qwr wre','2014-09-03 09:47:31');
-
 /*Table structure for table `st_orders` */
 
 DROP TABLE IF EXISTS `st_orders`;
@@ -119,7 +102,7 @@ CREATE TABLE `st_product2property` (
 
 /*Data for the table `st_product2property` */
 
-insert  into `st_product2property`(`idProduct`,`idProperty`,`value`) values (1,1,'3000'),(1,2,'2'),(1,3,'6'),(2,1,'5000'),(2,2,'6'),(2,3,'10'),(3,1,'2500'),(3,2,'3'),(3,3,'2'),(7,4,'400'),(7,5,'1024'),(8,4,'300'),(8,5,'512'),(10,4,'500'),(10,5,'1024'),(12,6,'hdd'),(12,7,'1024'),(12,8,'24'),(14,6,'hdd'),(14,7,'512'),(14,8,'6'),(13,6,'ssd'),(13,7,'512'),(13,8,'10'),(15,9,'LGA775, LGA1366, LGA1150, LGA1155, LGA1156, AM2, AM2+, AM3, AM3+, FM1, FM2'),(15,10,'21 dBA '),(16,9,'LGA775, LGA1366, LGA1155, LGA1156, AM2, AM2+, AM3'),(16,10,'19.8 dBA '),(17,9,''),(17,10,'20.5 dBA '),(18,11,'Intel \r\n'),(18,9,' LGA1366'),(20,11,'AMD '),(20,9,'AM3 '),(21,11,'Intel '),(21,9,'LGA1155 '),(22,7,'1024'),(22,1,'100'),(23,7,'512'),(23,1,'200'),(24,7,'2048'),(24,1,'300'),(25,12,'mini-ATX '),(25,13,'top'),(26,12,''),(26,13,''),(28,12,''),(28,13,'bottom'),(29,14,'500'),(30,14,''),(32,14,'750'),(71,1,'46'),(71,2,'465'),(71,3,'46');
+insert  into `st_product2property`(`idProduct`,`idProperty`,`value`) values (1,1,'3000'),(1,2,'2'),(1,3,'6'),(2,1,'5000'),(2,2,'6'),(2,3,'10'),(3,1,'2500'),(3,2,'3'),(3,3,'2'),(7,4,'400'),(7,5,'1024'),(8,4,'300'),(8,5,'512'),(10,4,'500'),(10,5,'1024'),(12,6,'hdd'),(12,7,'1024'),(12,8,'24'),(14,6,'hdd'),(14,7,'512'),(14,8,'6'),(13,6,'ssd'),(13,7,'512'),(13,8,'10'),(15,9,'LGA775, LGA1366, LGA1150, LGA1155, LGA1156, AM2, AM2+, AM3, AM3+, FM1, FM2'),(15,10,'21 dBA '),(16,9,'LGA775, LGA1366, LGA1155, LGA1156, AM2, AM2+, AM3'),(16,10,'19.8 dBA '),(17,9,''),(17,10,'20.5 dBA '),(18,11,'Intel \r\n'),(18,9,' LGA1366'),(20,11,'AMD '),(20,9,'AM3 '),(21,11,'Intel '),(21,9,'LGA1155 '),(22,7,'1024'),(22,1,'100'),(23,7,'512'),(23,1,'200'),(24,7,'2048'),(24,1,'300'),(25,12,'mini-ATX '),(25,13,'top'),(26,12,''),(26,13,''),(28,12,''),(28,13,'bottom'),(29,14,'500'),(30,14,''),(32,14,'750');
 
 /*Table structure for table `st_products` */
 
@@ -136,11 +119,11 @@ CREATE TABLE `st_products` (
   PRIMARY KEY (`id`),
   KEY `product2category` (`idCategory`),
   CONSTRAINT `product2category` FOREIGN KEY (`idCategory`) REFERENCES `st_category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8;
 
 /*Data for the table `st_products` */
 
-insert  into `st_products`(`id`,`name`,`description`,`price`,`idCategory`,`img`,`delite`) values (1,'Intel Core i5-4570','2013 г. (Q2) Core i5 \r\nHaswell ',5,1,'cpu/i5.jpg',NULL),(2,'Intel Core i7-4790K','2014 г. (Q2) Core i7 Devil\'s Canyon LGA1150 ',7,1,'cpu/i7.jpg',NULL),(3,'AMD A8-6600K','2013 г. (Q2) A8  Richland  FM2 ',4,1,'cpu/a8.jpg',NULL),(7,'MSI GeForce GTX 760','2013 г.  PCI Express x16 3.0  NVIDIA GeForce GTX 760 ',10,3,'videokart/gtx750ti.jpg',NULL),(8,'ASUS GeForce GTX 650 Ti','2012 г. PCI Express x16 3.0 NVIDIA GeForce GTX 650 Ti \r\n ',8,3,'videokart/gtx760oc.jpg',NULL),(10,'ASUS GeForce GTX 750 Ti','2014 г. \r\nPCI Express x16 3.0 NVIDIA GeForce GTX 750 Ti ',6,3,'videokart/gtx650.jpg',NULL),(12,'Seagate Enterprise Capacity 6TB','2014 г. \r\n',2,4,'hdd/enterprise.jpg',NULL),(13,'SSD Silicon-Power Extreme','2012 г. \r\n',3,4,'hdd/ssdSiliconPower.jpg',NULL),(14,'Seagate Barracuda 7200.14 1TB','2011 г. ',4,4,'hdd/seagateBarracuda.jpg',NULL),(15,'Thermalright HR-02 Macho','для процессора  воздушное ',1,5,'cooller/noctua.jpg',NULL),(16,'Noctua NH-D14','для процессора воздушное ',2,5,'cooller/thermalright.jpg',NULL),(17,'Xilence RedWing COO-XPF120.R','для корпуса воздушное ',1,5,'cooller/xilence.jpg',NULL),(18,'ASUS Rampage IV Extreme','2011 г. ',3,6,'mb/rampage.jpg',NULL),(20,'BIOSTAR N68S3B Ver. 6.x','',2,6,'mb/biostar.jpg',NULL),(21,'ASUS P8Z77-V LX','2012 г. ',4,6,'mb/p8z77.jpg',NULL),(22,'Hynix DDR2 PC2-6400 2 Гб','',2,7,'ram/hynix.jpg',NULL),(23,'Kingston HyperX Genesis','',3,7,'ram/kingston.jpg',NULL),(24,'Corsair Vengeance Blue 2x4GB','',3,7,'ram/corsair.jpg',NULL),(25,'Zalman Z11 plus Black','',5,8,'body/zalman.jpg',NULL),(26,'In Win GT1 BWR145 Black','',7,8,'body/bwr145.jpg',NULL),(28,'Cooler Master Elite 110','',8,8,'body/masterElite.jpg',NULL),(29,'Zalman ZM500-LE 500W','',5,9,'powerSupply/zalman.jpg',NULL),(30,'Chieftec Navitas GPM-850C','',5,9,'powerSupply/750c.jpg',NULL),(32,'Chieftec A-135 APS-750CB','',7,9,'powerSupply/сhieftec.jpg',NULL),(33,'ASUS GeForce GTX TITAN Z 12GB GDDR5','2014 г. PCI Express x16 3.0 ',20,3,'videokart/GeForceGtxTITAN.jpg',NULL),(34,'Palit GeForce GTX 760 JETSTREAM 4GB GDDR5','2013 г PCI Express x16 3.0 ',10,3,'videokart/GTX760.jpg',NULL),(71,'46','46',465,1,'img/',NULL);
+insert  into `st_products`(`id`,`name`,`description`,`price`,`idCategory`,`img`,`delite`) values (1,'Intel Core i5-4570','2013 г. (Q2) Core i5 \r\nHaswell ',5,1,'cpu/i5.jpg',NULL),(2,'Intel Core i7-4790K','2014 г. (Q2) Core i7 Devil\'s Canyon LGA1150 ',7,1,'cpu/i7.jpg',NULL),(3,'AMD A8-6600K','2013 г. (Q2) A8  Richland  FM2 ',4,1,'cpu/a8.jpg',NULL),(7,'MSI GeForce GTX 760','2013 г.  PCI Express x16 3.0  NVIDIA GeForce GTX 760 ',10,3,'videokart/gtx750ti.jpg',NULL),(8,'ASUS GeForce GTX 650 Ti','2012 г. PCI Express x16 3.0 NVIDIA GeForce GTX 650 Ti \r\n ',8,3,'videokart/gtx760oc.jpg',NULL),(10,'ASUS GeForce GTX 750 Ti','2014 г. \r\nPCI Express x16 3.0 NVIDIA GeForce GTX 750 Ti ',6,3,'videokart/gtx650.jpg',NULL),(12,'Seagate Enterprise Capacity 6TB','2014 г. \r\n',2,4,'hdd/enterprise.jpg',NULL),(13,'SSD Silicon-Power Extreme','2012 г. \r\n',3,4,'hdd/ssdSiliconPower.jpg',NULL),(14,'Seagate Barracuda 7200.14 1TB','2011 г. ',4,4,'hdd/seagateBarracuda.jpg',NULL),(15,'Thermalright HR-02 Macho','для процессора  воздушное ',1,5,'cooller/noctua.jpg',NULL),(16,'Noctua NH-D14','для процессора воздушное ',2,5,'cooller/thermalright.jpg',NULL),(17,'Xilence RedWing COO-XPF120.R','для корпуса воздушное ',1,5,'cooller/xilence.jpg',NULL),(18,'ASUS Rampage IV Extreme','2011 г. ',3,6,'mb/rampage.jpg',NULL),(20,'BIOSTAR N68S3B Ver. 6.x','',2,6,'mb/biostar.jpg',NULL),(21,'ASUS P8Z77-V LX','2012 г. ',4,6,'mb/p8z77.jpg',NULL),(22,'Hynix DDR2 PC2-6400 2 Гб','',2,7,'ram/hynix.jpg',NULL),(23,'Kingston HyperX Genesis','',3,7,'ram/kingston.jpg',NULL),(24,'Corsair Vengeance Blue 2x4GB','',3,7,'ram/corsair.jpg',NULL),(25,'Zalman Z11 plus Black','',5,8,'body/zalman.jpg',NULL),(26,'In Win GT1 BWR145 Black','',7,8,'body/bwr145.jpg',NULL),(28,'Cooler Master Elite 110','',8,8,'body/masterElite.jpg',NULL),(29,'Zalman ZM500-LE 500W','',5,9,'powerSupply/zalman.jpg',NULL),(30,'Chieftec Navitas GPM-850C','',5,9,'powerSupply/750c.jpg',NULL),(32,'Chieftec A-135 APS-750CB','',7,9,'powerSupply/750.jpg',NULL),(33,'ASUS GeForce GTX TITAN Z 12GB GDDR5','2014 г. PCI Express x16 3.0 ',20,3,'videokart/GeForceGtxTITAN.jpg',NULL),(34,'Palit GeForce GTX 760 JETSTREAM 4GB GDDR5','2013 г PCI Express x16 3.0 ',10,3,'videokart/GTX760.jpg',NULL);
 
 /*Table structure for table `st_properties` */
 
